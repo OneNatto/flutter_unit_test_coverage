5a1db2095180a4a57115d669d052e07094cd44f0
OneNatto <segaryo0204@icloud.com>
2025-12-27 17:24:14 +0900

    :sparkle: Make Calculator Class

---
+++ lib/calculator.dart
@@ -0,0 +1,16 @@
+class Calculator {
+  // 足し算を行うメソッド
+  int add(int a, int b) {
+    return a + b;
+  }
+
+  // 引き算を行うメソッド
+  int subtract(int a, int b) {
+    return a - b;
+  }
+
+  // 掛け算を行うメソッド
+  int multiply(int a, int b) {
+    return a * b;
+  }
+}
